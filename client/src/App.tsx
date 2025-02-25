3a5b6d7aabba515af1810a4ab122c8838bea7148
Han Hsun Shih (Amy) <a23514788@gmail.com>
2025-02-25 01:01:06 +0000

    Check if jd field is null, if so, show error message

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,48 @@
 // import { useState } from "react";
 import "./App.scss";
+import axios from "axios";
 import docImg from "./assets/images/doc.png";
 import { getOpenAIResponse } from "../utils/apiUtils";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
   const [result, setResult] = useState("");
+  const jdInput = useRef<HTMLInputElement>(null);
+  const [jdErrorMessage, setJdErrorMessage] = useState("");
 
   const responseRender = async () => {
     try {
       const response = await getOpenAIResponse();
-      setResult(response);
+      setResult(response.content);
     } catch (error) {
       console.log(error);
     }
   };
 
-  const handleClick = () => {};
+  const handleJdOnChange = () => {
+    setJdErrorMessage("");
+  };
+
+  console.log("result: ");
+  console.log(result);
+
+  const handleSubmitForm = (event: React.FormEvent<HTMLFormElement>) => {
+    try {
+      event.preventDefault();
+
+      if (!jdInput.current?.value.trim()) {
+        setJdErrorMessage("Please paste job description");
+      } else {
+        // axios.
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    responseRender();
+  }, []);
 
   return (
     <>
@@ -24,13 +50,21 @@ function App() {
         <header className="header">
           <h1 className="header__title">Resume Generator</h1>
         </header>
-        <form action="" className="form">
+        <form action="" className="form" onSubmit={handleSubmitForm}>
           <div className="form-group">
             <label htmlFor="link" className="form-group__label">
               Paste Job Description
             </label>
-            <input type="text" name="link" className="form-group__input" />
-
+            <input
+              type="text"
+              name="link"
+              className="form-group__input"
+              ref={jdInput}
+              onChange={handleJdOnChange}
+            />
+            {jdErrorMessage && (
+              <p className="form__errorMessage">{jdErrorMessage}</p>
+            )}
             <label htmlFor="resumeSubmitted" className="form-group__label">
               Choose your CV from device
             </label>
@@ -44,7 +78,6 @@ function App() {
               className="form-group__file-input"
               accept="application/pdf"
             />
-
             <button type="submit" className="form-group__button">
               Generate
             </button>
--- client/utils/apiUtils.ts
+++ client/utils/apiUtils.ts
@@ -6,7 +6,7 @@ const api_url = import.meta.env.VITE_SERVER_URL;
 export const getOpenAIResponse = async () => {
   try {
     const response = await axios.get(`${api_url}/openai`);
-    return response.data.contnet;
+    return response.data;
   } catch (error) {
     console.error("Error fetching products data" + error);
   }
