2cba9d3286fdc81d6eb5afcc53655226d8078071
Han Hsun Shih (Amy) <a23514788@gmail.com>
2025-05-01 01:04:22 +0100

    Add migrations file

--- server/knexfile.js
+++ server/knexfile.js
@@ -15,7 +15,6 @@ export default {
     },
     migrations: {
       directory: "./migrations",
-      extension: "ts",
     },
   },
 };
---
+++ server/migrations/20250430235257_generate_count.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function up(knex) {
+  return knex.schema.createTable("generate_count", (table) => {
+    table.increments("id");
+    table.timestamp("created_at");
+  });
+}
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export function down(knex) {
+  knex.schema.dropTable("generate_count");
+}
